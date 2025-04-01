953fad05155883af751a0aebf243b087cff60d7f
Null <117316014+ltseverydayyou@users.noreply.github.com>
2025-04-02 00:12:14 +0300

    Update

---
+++ Delulu Office.lua
@@ -0,0 +1,33 @@
+if game.GameId==6352299542 then
+local rs = game:GetService("ReplicatedStorage")
+local p = game:GetService("Players")
+local lp = p.LocalPlayer
+local c = lp.Character
+local hm = c:FindFirstChild("HealthManager", true)
+local r = rs:WaitForChild("Remotes")
+local dc = r:WaitForChild("DamageCall")
+local runService = game:GetService("RunService")
+local a = { -math.huge }
+
+local function hp()
+    dc:FireServer(unpack(a))
+end
+
+hm:GetPropertyChangedSignal("Value"):Connect(hp)
+
+runService.RenderStepped:Connect(function()
+task.spawn(hp)
+end)
+if hookmetamethod then
+local namecall
+namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    local method = getnamecallmethod():lower()
+    if not checkcaller() and self == dc and (method == "invokeserver" or method == "fireserver") then
+        local args = {...}
+        args[1] = -math.huge
+        return namecall(self, unpack(args))
+    end
+    return namecall(self, ...)
+end)
+end
+end
